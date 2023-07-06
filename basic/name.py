93d24e39b44db7cf979e6897909f5b60378b2512
Farhad-Sayed <sayedtheprogrammer@gmail.com>
2023-07-06 15:49:15 +0600

    Initial commit from my pycharm!

---
+++ basic/name.py
@@ -0,0 +1,10 @@
+# problem:
+# This program is a simple program which will not end until you input the correct name!
+
+
+name = ''
+
+while name != 'Sayed'.lower():
+    print('Please type your name: ')
+    name = input()
+print('Thank you!', name)
