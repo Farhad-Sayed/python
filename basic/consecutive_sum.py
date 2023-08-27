db384785803781b8645c4d9b0e89ba988bbc902c
Farhad-Sayed <sayedtheprogrammer@gmail.com>
2023-08-27 06:11:27 +0600

    add consecutive sum using range function!

--- basic/consecutive_sum.py
+++ basic/consecutive_sum.py
@@ -3,3 +3,12 @@ total = 0
 for num in range(101):
     total += num
 print(total)
+
+# consecutive sum using while loop:
+
+# total = 0
+# num = 0
+# while num < 101:
+#     total += num
+#     num += 1
+# print(total)
