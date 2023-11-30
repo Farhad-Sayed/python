0fdd59eb869e6cef133ab2e65254bda53781ecca
Farhad-Sayed <sayedtheprogrammer@gmail.com>
2023-11-30 09:16:31 +0600

    Learned How 'end' and 'sep' work as keyowrd argumnets

---
+++ basic/keyword_arguments.py
@@ -0,0 +1,10 @@
+# Keyword arguments are identified by the keyword put before them in the function call.
+# Keyword arguments are optional parameters.
+# For example, the print() function has the optional parameters 'end' and 'sep' to specify what should be printed
+# at the end of its arguments and between its arguments (separating them) respectively.
+
+print('Hello', end='')
+print('World!')
+
+print('cats', 'dogs', 'mice')
+print('cats', 'dogs', 'mice', sep=',')
