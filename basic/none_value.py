38e3814846ff50519dacf832c167c511844bf83c
Farhad-Sayed <sayedtheprogrammer@gmail.com>
2023-11-30 08:59:53 +0600

    Learning None Value!

---
+++ basic/none_value.py
@@ -0,0 +1,6 @@
+# In Python 'None' value is the absence of value.
+# This value-without-a-value can be helpful when we need to store something that won't be confused for a real value.
+# Behind the scene, Python adds 'return' 'None' to the end of any function definition with no return statement.
+
+spam = print('None Value!')
+print(spam == None)
